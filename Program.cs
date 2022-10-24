db028718e1206afb63665a427ba9c82477f43852
Hugh-l-C <hc58@aru.ac.uk>
2022-10-24 19:12:19 +0100

    new

--- Program.cs
+++ Program.cs
@@ -2,7 +2,7 @@
 while(pet.Alive)
 {
     Console.WriteLine($"Health {pet.health}");
-    Console.WriteLine("Specify what you want");
+    Console.WriteLine("Specify what you want!");
     Console.WriteLine("Menu");
     string userChoice = Console.ReadLine();
 }
