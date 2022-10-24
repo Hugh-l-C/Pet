913bc10d01d59d7ba6d54dab37ca2a86872cf27a
Hugh-l-C <hc58@aru.ac.uk>
2022-10-24 19:22:09 +0100

    update

--- Program.cs
+++ Program.cs
@@ -1,8 +1,8 @@
 ﻿Animal pet = new Animal();
 while(pet.Alive)
 {
-    Console.WriteLine($"Health {pet.health}!");
-    Console.WriteLine("Specify what you want!");
+    Console.WriteLine($"Health {pet.health}");
+    Console.WriteLine("Specify what you want");
     Console.WriteLine("Menu");
     string userChoice = Console.ReadLine();
 }
