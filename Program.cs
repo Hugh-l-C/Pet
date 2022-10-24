0f2b03cde8a9e184cfba54a3f781ce0e9983e44c
Hugh-l-C <hc58@aru.ac.uk>
2022-10-24 19:24:03 +0100

    Done

--- Program.cs
+++ Program.cs
@@ -4,5 +4,6 @@ while(pet.Alive)
     Console.WriteLine($"Health {pet.health}");
     Console.WriteLine("Specify what you want");
     Console.WriteLine("Menu");
+    //This is where the menu would go
     string userChoice = Console.ReadLine();
 }
